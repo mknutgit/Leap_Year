be767b364d0da3447258b64baed09a12d1568ff5
Matt Knutson <mattknutson@Matts-MacBook-Pro.local>
2016-01-19 14:42:53 -0800

    Bower bootstrap added, css updated function for leap year refactored

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 var leapYear = function(year) {
-  return ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
+ return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+
 };
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!leapYear(year)) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
